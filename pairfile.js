94f2a5f72fdfc8a743ff82cbb4b8a507e6c83ad8
Princeshoro <115966212+Princeshoro@users.noreply.github.com>
2024-07-25 11:22:43 +0500

    Update pairfile.js

--- pairfile.js
+++ pairfile.js
@@ -1,84 +1,86 @@
+PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const {makeid} = require('./id');
 const express = require('express');
 const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: makeWASocket,
+    default: PRINCE_GDS,
     useMultiFileAuthState,
     delay,
-    makeCacheableSignalKeyStore
+    makeCacheableSignalKeyStore,
+    Browsers
 } = require("@whiskeysockets/baileys");
+
 
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
     fs.rmSync(FilePath, { recursive: true, force: true })
  };
 router.get('/', async (req, res) => {
+    const id = makeid();
     let num = req.query.number;
-        async function XeonPair() {
+        async function PRINCE_MD_PAIR_CODE() {
         const {
             state,
             saveCreds
-        } = await useMultiFileAuthState(`./session`)
+        } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let XeonBotInc = makeWASocket({
+            let Pair_Prince = PRINCE_GDS({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: [ "Ubuntu", "Chrome", "20.0.04" ],
+                browser: ["Chrome (Linux)", "Chrome (Ubuntu)", ""]
              });
-             if(!XeonBotInc.authState.creds.registered) {
+             if(!Pair_Prince.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await XeonBotInc.requestPairingCode(num)
+                            const code = await Pair_Prince.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            XeonBotInc.ev.on('creds.update', saveCreds)
-            XeonBotInc.ev.on("connection.update", async (s) => {
+            Pair_Prince.ev.on('creds.update', saveCreds)
+            Pair_Prince.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
                 } = s;
                 if (connection == "open") {
-                await delay(10000);
-                    const sessionXeon = fs.readFileSync('./session/creds.json');
-                    XeonBotInc.groupAcceptInvite("Jo5bmHMAlZpEIp75mKbwxP");
-				const xeonses = await XeonBotInc.sendMessage(XeonBotInc.user.id, { document: sessionXeon, mimetype: `application/json`, fileName: `creds.json` });
-				await XeonBotInc.sendMessage(XeonBotInc.user.id, { text: `You have successfully paired Here is your SESSION file upload it in Session folder and deploy your bot\nDo not share this file with anybody\n\nPRINCE-GDS ` }, {quoted: xeonses});
+                await delay(5000);
+                let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                await delay(1000);
+	       const xeonses = await Pair_Prince.sendMessage(Pair_Prince.user.id, { document: data, mimetype: `application/json`, fileName: `creds.json` });
+               Pair_Prince.groupAcceptInvite("Jo5bmHMAlZpEIp75mKbwxP");
+               let session = await Pair_Prince.sendMessage(Pair_Prince.user.id, { text: '' + xeonses });
+
+               let PRINCE_MD_TEXT = `𝗣𝗥𝗜𝗡𝗖𝗘-𝗚𝗗𝗦 
+               
+               😍𝙔𝙤𝙪 𝙝𝙖𝙫𝙚 𝙨𝙪𝙘𝙘𝙚𝙨𝙨𝙛𝙪𝙡𝙡𝙮 𝙥𝙖𝙞𝙧𝙚𝙙 𝙃𝙚𝙧𝙚 𝙞𝙨 𝙮𝙤𝙪𝙧 𝙎𝙀𝙎𝙎𝙄𝙊𝙉 
+               𝙘𝙤𝙥𝙮 𝙞𝙩 𝙖𝙣𝙙 𝙙𝙚𝙥𝙡𝙤𝙮 𝙮𝙤𝙪𝙧 𝙗𝙤𝙩`
+ await Pair_Prince.sendMessage(Pair_Prince.user.id,{text:PRINCE_MD_TEXT},{quoted:session})
+ 
+
         await delay(100);
-        return await removeFile('./session');
-        process.exit(0)
+        await Pair_Prince.ws.close();
+        return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    XeonPair();
+                    PRINCE_MD_PAIR_CODE();
                 }
             });
         } catch (err) {
             console.log("service restated");
-            await removeFile('./session');
+            await removeFile('./temp/'+id);
          if(!res.headersSent){
             await res.send({code:"Service Unavailable"});
          }
         }
     }
-    return await XeonPair()
+    return await PRINCE_MD_PAIR_CODE()
 });
-
-process.on('uncaughtException', function (err) {
-let e = String(err)
-if (e.includes("conflict")) return
-if (e.includes("Socket connection timeout")) return
-if (e.includes("not-authorized")) return
-if (e.includes("rate-overlimit")) return
-if (e.includes("Connection Closed")) return
-if (e.includes("Timed Out")) return
-if (e.includes("Value not found")) return
-console.log('Caught exception: ', err)
-})
-
 module.exports = router
