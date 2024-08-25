1dc57d918e7ba6cb2edf73eccde1199a267f7ec0
Princeshoro <115966212+Princeshoro@users.noreply.github.com>
2024-08-25 13:42:30 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -33,7 +33,7 @@ router.get('/', async (req, res) => {
                 },
                 printQRInTerminal: false,
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
-                browser: [ "Ubuntu", "Chrome", "20.0.04" ]
+                browser: Browsers.ubuntu("Chrome")
              });
              if(!Pair_Code_By_Maher_Zubair.authState.creds.registered) {
                 await delay(1500);
